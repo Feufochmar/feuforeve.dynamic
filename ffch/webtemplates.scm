0247ac101c58d8faf1f13db90e291ff01ed760fb
Feufochmar <feufochmar.gd@gmail.com>
2015-08-02 00:48:00 +0200

    Export metadata function and change the definition of webtemplate to use <header> and <footer> elements.

--- ffch/webtemplates.scm
+++ ffch/webtemplates.scm
@@ -6,8 +6,7 @@
   #:use-module (ffch article)
   #:use-module (ffch article-exporters sxml-html)
   #:use-module (sxml simple)
-  #:export (templated-weblet
-            webtemplate
+  #:export (templated-weblet metadata webtemplate
            )
 )
 
@@ -23,53 +22,47 @@
   ))
 
 ;; Html Metadata
-(define-class <html-metadata> (<object>)
+(define-class <metadata> (<object>)
   (stylesheets #:getter stylesheets #:init-keyword #:stylesheets #:init-form (list))
   (scripts #:getter scripts #:init-keyword #:scripts #:init-form (list)))
 
-(define-syntax html-metadata
+(define-syntax metadata
   (syntax-rules ()
     ((_ (slot values* ...) ...)
-     (let ((metadata (make <html-metadata>)))
+     (let ((meta (make <metadata>)))
        (begin
-         (slot-set! metadata (quote slot) (list values* ...)) ...
+         (slot-set! meta (quote slot) (list values* ...)) ...
        )
-       metadata))))
+       meta))))
 
 ;;
-(define-syntax webtemplate
-  (syntax-rules (metadata before after)
-   ((_ (metadata meta* ...)
-       (before-article bfr* ...)
-       (after-article aft* ...))
-    (let ((meta (html-metadata meta* ...)))
-      (lambda (art)
-        (list 'html
-          (list 'head
-            (list 'title (title art))
-            (list 'meta (list '@ (list 'charset "UTF-8")))
-            (map
-              (lambda (x)
-                (list 'link
-                  (list '@
-                    (list 'href x)
-                    (list 'rel "stylesheet")
-                    (list 'type "text/css")
-                    (list 'media "all")
-                  )))
-              (stylesheets meta))
-            (map
-              (lambda (x)
-                (list 'script
-                  (list '@
-                    (list 'src x)
-                  )))
-              (scripts meta))
-          )
-          (list 'body
-            (list 'div (list bfr* ...))
-            (article->sxml-html art)
-            (list 'div (list aft* ...))
-          )
-        )))
-   )))
+(define-method (webtemplate (meta <metadata>) (before <header>) (after <footer>))
+  (lambda (art)
+    (list 'html
+      (list 'head
+        (list 'title (title art))
+        (list 'meta (list '@ (list 'charset "UTF-8")))
+        (map
+          (lambda (x)
+            (list 'link
+              (list '@
+                (list 'href x)
+                (list 'rel "stylesheet")
+                (list 'type "text/css")
+                (list 'media "all")
+              )))
+          (stylesheets meta))
+        (map
+          (lambda (x)
+            (list 'script
+              (list '@
+                (list 'src x)
+              )))
+          (scripts meta))
+      )
+      (list 'body
+        (article->sxml-html before)
+        (article->sxml-html art)
+        (article->sxml-html after)
+      )
+    )))
