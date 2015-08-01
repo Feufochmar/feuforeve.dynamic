4287c00cbd931699980adfc484fb49e8606d8649
Feufochmar <feufochmar.gd@gmail.com>
2015-08-01 17:03:32 +0200

    Add support of Location header. Change how Content-Type header is processed.

--- ffch/weblets.scm
+++ ffch/weblets.scm
@@ -5,6 +5,7 @@
   #:use-module (web server)
   #:use-module (web request)
   #:use-module (web response)
+  #:use-module (web http)
   #:use-module (web uri)
   #:export (weblet webcontainer
             add-weblet run-webcontainer
@@ -13,9 +14,10 @@
 
 ;; Weblet : web page handling
 (define-class <weblet> (<object>)
-  (content-type #:getter content-type #:init-keyword #:content-type #:init-form 'text/html)
+  (content-type #:getter content-type #:init-keyword #:content-type #:init-form #f)
   (output-writer #:getter output-writer #:init-keyword #:output-writer #:init-from (lambda (path query port) port))
   (error-code #:getter error-code #:init-keyword #:error-code #:init-form 200)
+  (location #:getter location #:init-keyword #:location #:init-form #f)
 )
 
 ;; Web container : the webserver
@@ -23,20 +25,45 @@
   (server-port #:getter server-port #:init-keyword #:server-port #:init-form 8080)
   (weblets #:getter weblets #:init-form (make-hash-table))) ;; map of string -> weblet
 
+;;;;
 ;; Weblet methods
+
+;; Constructor
 (define-syntax weblet
   (syntax-rules ()
    ((_ ((slot value) ...) ((params* ...) cmd cmd* ...))
     (let ((wlet (make <weblet> #:output-writer (lambda (params* ...) cmd cmd* ...))))
       (begin
-        (slot-set! wlet (quote slot) value) ...
+        (weblet-slot-set! wlet (quote slot) value) ...
       )
       wlet
     ))))
 
+;; Slot set
+(define-method (weblet-slot-set! (wlet <weblet>) (slot <symbol>) value)
+  (cond
+    ((eq? slot 'error-code) (slot-set! wlet 'error-code value))
+    ((eq? slot 'content-type) (slot-set! wlet 'content-type (parse-header 'content-type value)))
+    ((eq? slot 'location) (slot-set! wlet 'location (parse-header 'location value)))
+    (#t (error "Unkown weblet slot: " slot))))
+
+;; Build the header of a weblet
+(define-method (build-headers (wlet <weblet>))
+  (let ((headers (list)))
+    (if (content-type wlet)
+        (set! headers (cons (cons 'content-type (content-type wlet)) headers)))
+    (if (location wlet)
+        (set! headers (cons (cons 'location (location wlet)) headers)))
+    headers))
+
+;;;;
 ;; Webcontainer methods
+
+;; Constructor
 (define-method (webcontainer (port <integer>))
   (make <webcontainer> #:server-port port))
+
+;; Add weblet to webcontainer
 (define-method (add-weblet (wcontainer <webcontainer>) (page <string>) (wlet <weblet>))
   (hash-set! (weblets wcontainer) page wlet))
 
@@ -70,7 +97,7 @@
               (if real-wlet
                   (values
                     (build-response #:code (error-code real-wlet)
-                                    #:headers `((content-type . (,(content-type real-wlet) (charset . "UTF-8")))))
+                                    #:headers (build-headers real-wlet))
                     (lambda (port)
                       (set-port-encoding! port "UTF-8")
                       ((output-writer real-wlet) path query port)
