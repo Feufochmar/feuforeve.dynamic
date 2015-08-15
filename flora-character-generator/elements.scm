cebc647eba942ce7c479d6445cf91f9fcb7bbf48
Feufochmar <feufochmar.gd@gmail.com>
2015-08-15 17:44:59 +0200

    Bring back element class. Add a method to get all element keys.

--- flora-character-generator/elements.scm
+++ flora-character-generator/elements.scm
@@ -3,9 +3,14 @@
 (define-module (flora-character-generator elements)
   #:version (0 0 1)
   #:use-module (oop goops)
-  #:export (get-element
+  #:export (<element> name key
+            get-element element-keys
            )
 )
+
+(define-class <element> (<object>)
+  (key #:getter key #:init-keyword #:key #:init-form #f)
+  (name #:getter name #:init-keyword #:name #:init-form ""))
 
 ;; Data singleton
 (define *data:elements* (make-hash-table))
@@ -13,12 +18,15 @@
 (define-method (get-element (key <symbol>))
   (hash-ref *data:elements* key))
 
+(define-method (element-keys)
+  (hash-map->list (lambda (k v) k) *data:elements*))
+
 ;; Damta syntax
 (define-syntax elements
   (syntax-rules ()
     ((_ (key val) ...)
      (begin
-       (hash-set! *data:elements* (quote key) val) ...))))
+       (hash-set! *data:elements* (quote key) (make <element> #:key (quote key) #:name val)) ...))))
 
 ;; Include the data to fill *data:elements*
 (include "data/elements.scm")
