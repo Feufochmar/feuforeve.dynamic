125fa8a56d56ce8b72c9c246fc2ce317e0a8076f
Guillaume Delacourt <feufochmar.gd@gmail.com>
2015-08-21 19:51:38 +0200

    Add a way to display a real quote from its date in ArnYtron.

--- arnytron/arnytron.scm
+++ arnytron/arnytron.scm
@@ -20,30 +20,34 @@
 ;; Arnytron
 (define-class <arnytron> (<object>)
   (citations #:getter citations #:init-keyword #:citations #:init-form (vector))
+  (citation-map #:getter citations-map #:init-keyword #:citation-map #:init-form (make-hash-table))
   (generator #:getter generator #:init-keyword #:generator #:init-form (markhov-chain 1))
 )
 
 ;; Constructor
 (define-method (arnytron)
-  (let ((generator (markhov-chain 1)))
-    (make <arnytron>
-      #:generator generator
-      #:citations
-        (list->vector
-          (map
-            (lambda (x)
-              (let ((use? (car x))
-                    (cite (caddr x)))
-                (if use? (add-example generator (string-split cite #\space)))
-                (make <citation>
-                  #:use-in-generator? use?
-                  #:date (cadr x)
-                  #:citation cite
-                  #:reactions (cadddr x))
-              ))
-            greetings))
-    )
-  ))
+  (let* ((generator (markhov-chain 1))
+         (tron
+           (make <arnytron>
+                 #:generator generator
+                 #:citations
+                   (list->vector
+                     (map
+                       (lambda (x)
+                         (let ((use? (car x))
+                               (cite (caddr x)))
+                           (if use? (add-example generator (string-split cite #\space)))
+                           (make <citation>
+                             #:use-in-generator? use?
+                             #:date (cadr x)
+                             #:citation cite
+                             #:reactions (cadddr x))))
+                       greetings)))))
+    (for-each
+      (lambda (x)
+        (hash-set! (citations-map tron) (date x) x))
+      (vector->list (citations tron)))
+    tron))
 
 ;; Generate a quote
 (define-method (generate-citation (arny <arnytron>))
@@ -52,3 +56,9 @@
 ;; Pick a citation
 (define-method (pick-citation (arny <arnytron>))
   (pick-from (citations arny)))
+
+;; Pick a citation from a date
+(define-method (pick-citation (arny <arnytron>) (date <string>))
+  (or (hash-ref (citations-map arny) date)
+      (pick-from (citations arny))))
+
