f9495448df7d4e0ff060b6b7d9db0b6ffc244196
Guillaume Delacourt <feufochmar.gd@gmail.com>
2015-08-23 23:28:55 +0200

    Signal error when not able to start bot

--- ircbot-floraverse.scm
+++ ircbot-floraverse.scm
@@ -135,8 +135,9 @@
       (display "Exception occurred: ")(display key)(display " ")(display args)(newline)))
   ; Restart after a timeout if exception occurred
   ; If too much exceptions occured, quit
-  (sleep 10)
   (set! nbfailures (+ 1 nbfailures))
+  (sleep (* 10 nbfailures))
   (if (< nbfailures 10)
-      (main args)))
+      (main args)
+      (error "Unable to start Floraverse IRC bot")))
 
