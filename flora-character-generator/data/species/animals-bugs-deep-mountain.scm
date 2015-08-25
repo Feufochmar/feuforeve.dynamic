9a9dba26eec0c3f31bacaa0725d75b8de3b1c6d7
Guillaume Delacourt <feufochmar.gd@gmail.com>
2015-08-25 20:14:36 +0200

    Curiosity Killed The Rat: Bugs of Deep Mountain

---
+++ flora-character-generator/data/species/animals-bugs-deep-mountain.scm
@@ -0,0 +1,84 @@
+;; Bugs of Deep Mountain
+(species leaf-faced-ant
+  (name "Leaf-Faced Ant")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species flyght
+  (name "Flyght")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species rotobug
+  (name "Rotobug")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species catfish-rabbit
+  (name "Catfish Rabbit")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species coral-spiderfish
+  (name "Coral Spiderfish")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species slimepillar
+  (name "Slimepillar")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species trunkate
+  (name "Trunkate")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (wild? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
+
+(species fuzzy-moth
+  (name "Fuzzy Moth")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (tribal? #t)
+  (isolated? #t)
+  (common-regions deep-mountain)
+  (distributions
+    (affinity (* 1) (air 80) (light 50) (earth 50) (clay 40) (acid 35) (poison 35)))
+  (reproduction
+    (common-pattern? #t)))
+
+(species sawhorse
+  (name "Sawhorse")
+  (reference-link "http://floraverse.com/comic/curiosity-killed-the-rat/347-bugs-of-deep-mountain/")
+  (only-in-common-regions? #t)
+  (tamable? #t)
+  (common-regions deep-mountain)
+  (reproduction
+    (common-pattern? #t)))
