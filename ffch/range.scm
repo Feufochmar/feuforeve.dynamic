572ce1c6821f2ef1877b038f8f3a9d1c94804e48
Feufochmar <feufochmar.gd@gmail.com>
2015-09-01 00:59:41 +0200

    Change range to semi-inclusive interval. Add a range-inclusive variant for the previous version.

--- ffch/range.scm
+++ ffch/range.scm
@@ -2,10 +2,21 @@
 (define-module (ffch range)
   #:version (0 0 1)
   #:use-module (oop goops)
-  #:export (range)
+  #:export (range range-inclusive)
 )
 
 (define-method (range (mn <number>) (mx <number>) (step <number>))
+  (letrec ((helper
+             (lambda (lst current)
+               (if (< current mx)
+                   (helper (cons current lst) (+ current step))
+                   (reverse lst)))))
+    (helper (list) mn)))
+
+(define-method (range (mn <number>) (mx <number>))
+  (range mn mx 1))
+
+(define-method (range-inclusive (mn <number>) (mx <number>) (step <number>))
   (letrec ((helper
              (lambda (lst current)
                (if (< current mn)
@@ -13,5 +24,5 @@
                    (helper (cons current lst) (- current step))))))
     (helper (list) mx)))
 
-(define-method (range (mn <number>) (mx <number>))
-  (range mn mx 1))
+(define-method (range-inclusive (mn <number>) (mx <number>))
+  (range-inclusive mn mx 1))
