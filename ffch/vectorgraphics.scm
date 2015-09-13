5cf8c15fcc57eadc2bd76ebb49c9b444d01cd89b
Guillaume Delacourt <feufochmar.gd@gmail.com>
2015-09-13 22:22:29 +0200

    Add a flag to hide an element by default.

--- ffch/vectorgraphics.scm
+++ ffch/vectorgraphics.scm
@@ -12,7 +12,7 @@
             ;
             <script> script
             ;
-            <vectorgraphics-content-type> on-focus-in on-focus-out on-activate
+            <vectorgraphics-content-type> visible? on-focus-in on-focus-out on-activate
             on-click on-mouse-down on-mouse-up on-mouse-over on-mouse-move on-mouse-out
             ;
             <area> area transforms
@@ -102,6 +102,7 @@
 ;;;;
 ;; Vector content type: add scriptable attributes
 (define-class <vectorgraphics-content-type> (<content-type>)
+  (visible? #:getter visible? #:init-keyword #:visible? #:init-form #t)
   (on-focus-in #:getter on-focus-in #:init-keyword #:on-focus-in #:init-form #f)
   (on-focus-out #:getter on-focus-out #:init-keyword #:on-focus-out #:init-form #f)
   (on-activate #:getter on-activate #:init-keyword #:on-activate #:init-form #f)
