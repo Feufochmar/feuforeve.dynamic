3e50a9f669c59dc293c3059cd3a4675f5f196a16
Feufochmar <feufochmar.gd@gmail.com>
2015-11-03 12:49:20 +0100

    Add animals from Althar Entry 1.

---
+++ flora-character-generator/data/species/animals-althar.scm
@@ -0,0 +1,38 @@
+;; Casca's journal - Althar entry 1
+(species pendragon
+  (name "Pendragon")
+  (reference-link "http://floraverse.com/comic/cascas-journal/377-althar-entry-1/")
+  (only-in-common-regions? #t)
+  (pet? #t)
+  (common-regions althar)
+  (reproduction
+    (common-pattern? #t)))
+
+(species bookwyrm
+  (name "Bookwyrm")
+  (reference-link "http://floraverse.com/comic/cascas-journal/377-althar-entry-1/")
+  (only-in-common-regions? #t)
+  (pet? #t)
+  (common-regions althar)
+  (reproduction
+    (common-pattern? #t)))
+
+(species vinedra
+  (name "Vinedra")
+  (reference-link "http://floraverse.com/comic/cascas-journal/377-althar-entry-1/")
+  (only-in-common-regions? #t)
+  (tamable? #t)
+  (common-regions althar)
+  (reproduction
+    (common-pattern? #t)))
+
+(species dyevern
+  (name "Dyevern")
+  (reference-link "http://floraverse.com/comic/cascas-journal/377-althar-entry-1/")
+  (only-in-common-regions? #t)
+  (pet? #t)
+  (tamable? #t)
+  (wild? #t)
+  (common-regions althar)
+  (reproduction
+    (common-pattern? #t)))
