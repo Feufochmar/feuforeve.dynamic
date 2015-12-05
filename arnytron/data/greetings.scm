7f142664dc1f81311f53a2321ffbc60335f94736
Feufochmar <feufochmar.gd@gmail.com>
2015-12-05 20:12:38 +0100

    ArnY quote. 2015-12-05

--- arnytron/data/greetings.scm
+++ arnytron/data/greetings.scm
@@ -469,4 +469,5 @@
  (#t "2015-11-29" "salut les anulingueurs!" ())
  (#t "2015-11-30" "salut les naines cubiques à cul de sumo!" ())
  (#t "2015-12-01" "salut les cancerologues lépreux dépistant les cancers de la prostate avec leurs 10 ah non 9... ah non.. 8 maintenant... plus que 7 doigts" ("katyucha: :)" "katyucha: bravo" "katyucha: superbe" "katyucha: clap clap" "katyucha la forme ArnY ?" "ArnY: plutot fallique" "katyucha: madame va être contente :)" "Balise: \"ew\"" "Balise: c'est tout ce que j'ai à dire à ce sujet"))
+ (#t "2015-12-05" "salut les mono-cellulaires du cerveau spongiforme au gland en fleur" ())
 ))
