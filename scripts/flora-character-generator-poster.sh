583e746a01ed574aa098052fdc2f4ec40750fca3
Feufochmar <feufochmar.gd@gmail.com>
2016-07-23 01:32:39 +0200

    Remove shell scripts for bot posting. Those will be implemented in a different way.

--- scripts/flora-character-generator-poster.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-CONTENT=$(curl -s http://localhost:8080/FloraCharacterGenerator/sendmail)
-ADDRESS="tumblr-secret-email-address@tumblr.com"
-echo "From: user@domain
-To: $ADDRESS
-$CONTENT
-" | sendmail $ADDRESS
-
