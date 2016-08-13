5adc0087a44744a2359050cfedabe54b3ce6f87e
Feufochmar <feufochmar.gd@gmail.com>
2016-08-13 22:56:12 +0200

    ArnY quote.

--- arnytron/data/greetings.scm
+++ arnytron/data/greetings.scm
@@ -525,4 +525,5 @@
  (#t "2016-07-27" "salut les vieux forniquateurs de routier obèses accros aux buritos pine de porc/bouse de vache" ())
  (#t "2016-07-30" "salut les suceurs de biroutes de bigorneaux" ())
  (#t "2016-08-08" "salut les gros pompiers coprophages fraise-vanille" ("rbs: c'est pas ici" "Zluglu: on diti vanille-fraise"))
+ (#t "2016-08-13" "salut les fermentés du cerveau" ())
 ))
