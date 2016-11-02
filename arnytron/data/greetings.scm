7b1e9c0b09a515c48973a01839a3880aa66b6f55
Feufochmar <feufochmar.gd@gmail.com>
2016-11-02 02:23:54 +0100

    ArnY quote

--- arnytron/data/greetings.scm
+++ arnytron/data/greetings.scm
@@ -551,4 +551,5 @@
  (#t "2016-09-30" "tirlipipon vilains petits cochons!" ())
  (#t "2016-10-03" "salut les glandulateurs poilifiés de l'embouchure analesque" ())
  (#t "2016-10-17" "salut lé trompes de salopes disexlics!" ())
+ (#t "2016-11-01" "salut les décalqués de la prostates exubérante!" ())
 ))
