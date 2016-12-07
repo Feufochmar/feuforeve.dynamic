8fc51d7c3a854911fe55f8146d5af81069a94845
Feufochmar <feufochmar.gd@gmail.com>
2016-12-07 20:14:13 +0100

    Fix the aurorian-fow species for the region

--- flora-character-generator/data/species/aurorian-fox.scm
+++ flora-character-generator/data/species/aurorian-fox.scm
@@ -3,7 +3,7 @@
   (reference-link "http://floraverse.com/wiki/species/aurorian_fox")
   (citizen? #t)
   (common-regions
-    auroria althar mew-york citrico-capital twin-luxuries national-port-of-owel kadath)
+    auroria althar mew-york-territories citrico-capital twin-luxuries national-port-of-owel kadath)
   (distributions
     (affinity (* 1) (water 30) (air 30) (ice 30) (storm 15)))
   (reproduction
