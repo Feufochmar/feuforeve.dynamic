f42c9f6cc652e8cd5d6e90cdbf0780357acbe736
Feufochmar <feufochmar.gd@gmail.com>
2016-12-27 21:43:15 +0100

    ArnY quotes

--- arnytron/data/greetings.scm
+++ arnytron/data/greetings.scm
@@ -553,5 +553,7 @@
  (#t "2016-10-17" "salut lé trompes de salopes disexlics!" ())
  (#t "2016-11-01" "salut les décalqués de la prostates exubérante!" ())
  (#t "2016-11-11" "salut les rotobites 4 temps à double injection anale" ())
+ (#t "2016-12-15" "salut les vieux caca de la veille!!" ("ArnY: +s tas de merde"))
  (#t "2016-12-27" "salut les glouglouteurs de perches anatomiques!" ())
+ (#t "2016-12-27" "resalut les cassés de la boite à caca" ())
 ))
